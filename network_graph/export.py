f5babc9d681a4d14c3e45d0e12f7e3a87ca9c35a
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-03 01:58:35 +0800

    Accept labelled tweets as input and output to graph

--- network_graph/export.py
+++ network_graph/export.py
@@ -1,6 +1,7 @@
 import os
 from datetime import timezone, timedelta, datetime
 import json
+import csv
 
 def read_directory(directory_name):
     files = []
@@ -32,8 +33,29 @@ def read_file(filename):
     return tweets
 
 
+def get_sentiment_file(filename):
+    relevance = dict()
+    sentiment = dict()
+
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            relevance[row['id']] = row['relevance']
+            sentiment[row['id']] = row['sentiment']
+
+    return relevance, sentiment
+
 # Read directory
-DIRECTORY = './tweet_period/input'
+DIRECTORY = './network_graph/input/tweets'
+LABELLED_TWEETS = './network_graph/input/sentiment.csv'
+
+relevance_labels, sentiment_labels = get_sentiment_file(LABELLED_TWEETS)
+assert len(relevance_labels.keys()) == 2612018
+assert len(sentiment_labels.keys()) == 2612018
+
+tweets = set(list(relevance_labels.keys()) + list(sentiment_labels.keys()))
+assert len(tweets) == 2612018
+
 convert_to_full_path = lambda p: '{}/{}'.format(DIRECTORY, p)
 files = list(map(convert_to_full_path, read_directory(DIRECTORY)))
 assert len(files) == 21888
@@ -86,11 +108,11 @@ def test_date_string(date_str, milestones=MILESTONES):
     return 4
 
 
-def read_and_export(filename):
+def read_and_export(filename, tweet_set, relevance_labels, sentiment_labels):
     tweets = read_file(filename)
 
     for t in tweets:
-        process_tweet(t)
+        process_tweet(tweet_set, relevance_labels, sentiment_labels, t)
 
 
 USERS_FILE = './network_graph/output/users.csv'
@@ -106,9 +128,9 @@ replies_file = open(REPLIES_FILE, 'w')
 relationship_file = open(RELATIONSHIP_FILE, 'w')
 
 users_file.write('userId:ID,link,preferred_username\n')
-tweets_file.write('tweetId:ID,link,posted_time,period\n')
-retweets_file.write('tweetId:ID,link,posted_time,period\n')
-replies_file.write('tweetId:ID,link,posted_time,period\n')
+tweets_file.write('tweetId:ID,link,posted_time,period,relevance,sentiment\n')
+retweets_file.write('tweetId:ID,link,posted_time,period,relevance,sentiment\n')
+replies_file.write('tweetId:ID,link,posted_time,period,relevance,sentiment\n')
 relationship_file.write(':START_ID,:END_ID,:TYPE\n')
 
 
@@ -116,15 +138,19 @@ is_reply = lambda tweet: 'inReplyTo' in tweet.keys() and tweet['inReplyTo']
 is_retweet = lambda tweet: tweet['verb'] == 'share'
 print_user = lambda user_id, user_link, user_preferred_username: users_file.write(
     '{},"{}","{}"\n'.format(user_id, user_link, user_preferred_username))
-print_tweet = lambda tweet_id, tweet_link, tweet_posted_time, tweet_period: tweets_file.write(
-    '"{}","{}",{},{}\n'.format(tweet_id, tweet_link, tweet_posted_time, tweet_period))
-print_retweet = lambda retweet_id, retweet_link, retweet_posted_time, retweet_period: retweets_file.write(
-    '"{}","{}",{},{}\n'.format(retweet_id, retweet_link, retweet_posted_time, retweet_period))
-print_reply = lambda reply_id, reply_link, reply_posted_time, reply_period: replies_file.write(
-    '"{}","{}",{},{}\n'.format(reply_id, reply_link, reply_posted_time, reply_period))
+print_tweet = lambda tweet_id, tweet_link, tweet_posted_time, tweet_period, relevance, sentiment: tweets_file.write(
+    '"{}","{}",{},{},{},{}\n'.format(tweet_id, tweet_link, tweet_posted_time, tweet_period, relevance, sentiment))
+print_retweet = lambda retweet_id, retweet_link, retweet_posted_time, retweet_period, relevance, sentiment: retweets_file.write(
+    '"{}","{}",{},{},{},{}\n'.format(retweet_id, retweet_link, retweet_posted_time, retweet_period, relevance, sentiment))
+print_reply = lambda reply_id, reply_link, reply_posted_time, reply_period, relevance, sentiment: replies_file.write(
+    '"{}","{}",{},{},{},{}\n'.format(reply_id, reply_link, reply_posted_time, reply_period, relevance, sentiment))
 
 
-def process_tweet(tweet):
+def process_tweet(labelled_set, relevance_labels, sentiment_labels, tweet):
+    tweet_id = tweet['id']
+    if tweet_id not in labelled_set:
+        return
+
     user_id = tweet['actor']['id']
     user_link = tweet['actor']['link']
     user_preferred_username = tweet['actor']['preferredUsername']
@@ -137,27 +163,29 @@ def process_tweet(tweet):
         user_preferred_username = tweet['object']['actor']['preferredUsername']
         print_user(user_id, user_link, user_preferred_username)
 
-    tweet_id = tweet['id']
     tweet_user = user_id
     tweet_link = tweet['link']
     tweet_posted_time = tweet['postedTime']
     tweet_period = test_date_string(tweet_posted_time)
 
+    tweet_relevance = relevance_labels[tweet_id]
+    tweet_sentiment = sentiment_labels[tweet_id]
+
     relationship_file.write('"{}","{}",{}\n'.format(tweet_user, tweet_id, 'POSTED'))
     if is_retweet(tweet):
         retweet_of = tweet['object']['id']
-        print_retweet(tweet_id, tweet_link, tweet_posted_time, tweet_period)
+        print_retweet(tweet_id, tweet_link, tweet_posted_time, tweet_period, tweet_relevance, tweet_sentiment)
         relationship_file.write('"{}","{}",{}\n'.format(tweet_id, retweet_of, 'OF'))
     elif is_reply(tweet):
         reply_link = tweet['inReplyTo']['link']
         reply_to = 'tag:search.twitter.com,2005:' + reply_link.split('/')[5]
-        print_reply(tweet_id, tweet_link, tweet_posted_time, tweet_period)
+        print_reply(tweet_id, tweet_link, tweet_posted_time, tweet_period, tweet_relevance, tweet_sentiment)
         relationship_file.write('"{}","{}",{}\n'.format(tweet_id, reply_to, 'REPLY_TO'))
     else:
-        print_tweet(tweet_id, tweet_link, tweet_posted_time, tweet_period)
+        print_tweet(tweet_id, tweet_link, tweet_posted_time, tweet_period, tweet_relevance, tweet_sentiment)
 
 for f in files:
-    read_and_export(f)
+    read_and_export(f, tweets, relevance_labels, sentiment_labels)
     dot()
 
 
